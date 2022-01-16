e2a74f7a90a974bf093528c1a3e4660de2cc4616
enteryournamehere <11255568+enteryournamehere@users.noreply.github.com>
2022-01-16 18:35:18 +0100

    Convert without template

--- modules/fdb/examples/sqlite-to-fdb.rs
+++ modules/fdb/examples/sqlite-to-fdb.rs
@@ -1,6 +1,6 @@
-use assembly_fdb::{common::ValueType, core::Field, mem::Database, store};
+use assembly_fdb::{common::Latin1String, common::ValueType, core::Field, store};
 use color_eyre::eyre::{self, eyre, WrapErr};
-use mapr::Mmap;
+
 use rusqlite::{types::ValueRef, Connection};
 use std::{fmt::Write, fs::File, io::BufWriter, path::PathBuf, time::Instant};
 use structopt::StructOpt;
@@ -8,8 +8,6 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 /// Fills a template FDB file (see template-fdb.rs) with rows from a sqlite database
 struct Options {
-    /// "Template" FDB file, the table structure from this will be used for the output
-    template: PathBuf,
     /// Input sqlite database
     src: PathBuf,
     /// Output FDB file
@@ -23,13 +21,6 @@ fn main() -> eyre::Result<()> {
 
     println!("Converting database, this may take a few seconds...");
 
-    // fdb template
-    let template_file = File::open(&opts.template)
-        .wrap_err_with(|| format!("Failed to open fdb template '{}'", opts.template.display()))?;
-    let mmap = unsafe { Mmap::map(&template_file)? };
-    let buffer: &[u8] = &mmap;
-    let template_db = Database::new(buffer);
-
     // sqlite input
     let conn = Connection::open_with_flags(&opts.src, rusqlite::OpenFlags::SQLITE_OPEN_READ_ONLY)
         .wrap_err_with(|| format!("Failed to open sqlite file '{}'", opts.src.display()))?;
@@ -40,25 +31,53 @@ fn main() -> eyre::Result<()> {
     let mut dest_out = BufWriter::new(dest_file);
     let mut dest_db = store::Database::new();
 
-    for template_table in template_db.tables()?.iter() {
-        let template_table = template_table?;
+    let tables_query = String::from("select name from sqlite_master where type='table'");
+    let mut statement = conn.prepare(&tables_query)?;
+    let table_names = statement.query_map::<String, _, _>(rusqlite::NO_PARAMS, |row| row.get(0))?;
+
+    for table_name in table_names {
+        let table_name = table_name?;
+
+        println!("");
+        println!("Processing table '{}'", table_name);
+
+        // Build select query with the same column names and order as the template FDB
+        let mut select_query = String::from("select * from ");
+        select_query.push_str(&table_name);
+
+        // Prepare statement
+        let mut statement = conn.prepare(&select_query)?;
+
+        // Number of columns destination table should have
+        let column_count = statement.columns().len();
+
+        // Vector to store target datatypes in as these can't be determined from the sqlite source db
+        let mut target_types: Vec<ValueType> = Vec::with_capacity(column_count);
+
+        // Get column types
+        for column in &statement.columns() {
+            let decl_type = column.decl_type().expect("Failed to get column type");
+
+            let target_type =
+                ValueType::from_sqlite_type(decl_type).expect("Failed to convert column type");
+
+            target_types.push(target_type);
+
+            println!("Column '{}' has type '{}'", column.name(), target_type);
+        }
 
         // Find number of unique values in first column of source table
         let unique_key_count = conn.query_row::<u32, _, _>(
             &format!(
                 "select count(distinct [{}]) as unique_count from {}",
-                template_table
-                    .column_at(0)
-                    .ok_or_else(|| eyre!(format!(
-                        "FDB template contains no columns for table {}",
-                        template_table.name()
-                    )))?
-                    .name(),
-                template_table.name()
+                statement.columns().get(0).unwrap().name(),
+                table_name
             ),
             rusqlite::NO_PARAMS,
             |row| row.get(0),
         )?;
+
+        println!("Number of unique keys: {}", unique_key_count);
 
         // Bucket count should be 0 or a power of two
         let new_bucket_count = if unique_key_count == 0 {
@@ -70,35 +89,12 @@ fn main() -> eyre::Result<()> {
         // Create destination table
         let mut dest_table = store::Table::new(new_bucket_count as usize);
 
-        // Number of columns destination table should have
-        let column_count = template_table.column_count();
-
-        // Vector to store target datatypes in as these can't be determined from the sqlite source db
-        let mut target_types: Vec<ValueType> = Vec::with_capacity(column_count);
-
-        // Build select query with the same column names and order as the template FDB
-        let mut select_query = String::from("select ");
-
-        // Write select query and store target datatypes
-        for (index, template_column) in template_table.column_iter().enumerate() {
-            let template_column_name = template_column.name();
-
-            write!(select_query, "[{}]", template_column_name)?;
-
-            if index < column_count - 1 {
-                write!(select_query, ", ")?;
-            }
-
-            target_types.push(template_column.value_type());
-
-            dest_table.push_column(template_column.name_raw(), template_column.value_type());
+        // Add columns to destination table
+        for (i, column) in statement.columns().iter().enumerate() {
+            dest_table.push_column(Latin1String::encode(column.name()), *target_types.get(i).unwrap());
         }
 
-        select_query.push_str(" from ");
-        select_query.push_str(&template_table.name());
-
         // Execute query
-        let mut statement = conn.prepare(&select_query)?;
         let mut rows = statement.query(rusqlite::NO_PARAMS)?;
 
         // Iterate over rows
@@ -149,7 +145,7 @@ fn main() -> eyre::Result<()> {
             dest_table.push_row(pk, &fields);
         }
 
-        dest_db.push_table(template_table.name_raw(), dest_table);
+        dest_db.push_table(Latin1String::encode(&table_name), dest_table);
     }
 
     dest_db
